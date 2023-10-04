b3aa00fbed25e4c3bf64494c18600eb4e251890a
KolaYAndr <nikolay_andreev02@mail.ru>
2023-10-04 18:32:48 +0300

    small changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ import classes.Hero
 import classes.Monster
 import classes.MonsterType
 
-fun main(args: Array<String>) {
-    val hero = Hero(10/, 45, 6..10, 30)
+fun main() {
+    val hero = Hero(10, 45, 6..10, 30)
     val zombie = Monster.getMonster(MonsterType.ZOMBIE)
     val ghoul = Monster.getMonster(MonsterType.GHOUL)
     val wolf = Monster.getMonster(MonsterType.WEREWOLF)
